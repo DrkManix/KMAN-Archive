f74e010048bad05705338f4c55bf9c48ddff2ae7
DrkManix <45601260+DrkManix@users.noreply.github.com>
2020-06-01 10:03:16 +0200

    newdesignschange

--- js/main.js
+++
@@ -1,7 +0,0 @@
-document.getElementById('section0').style.backgroundImage="url(img/briefcase-cover1.jpg)";
-document.getElementById('section1').style.backgroundImage="url(img/briefcase-inside1.jpg)";
-document.getElementById('section2').style.backgroundImage="url(img/briefcase-inside1.jpg)";
-document.getElementById('section3').style.backgroundImage="url(img/briefcase-inside1.jpg)";
-document.getElementById('section4').style.backgroundImage="url(img/briefcase-inside1.jpg)";
-document.getElementById('section5').style.backgroundImage="url(img/briefcase-inside1.jpg)";
-document.getElementById('section6').style.backgroundImage="url(img/briefcase-inside1.jpg)";
